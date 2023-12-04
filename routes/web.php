34ff6b8f6af3008147cf6feb04fc454d28f16b67
Evan <evanrhaldeman@gmail.com>
2023-12-04 09:49:59 -0500

    more Blade fo you

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use App\Http\Controllers\doctorAppointmentController;
 use App\Http\Controllers\adminNavigationController;
 use App\Http\Controllers\RosterController;
 use App\Http\Controllers\DoctorHomeController;
+use App\Http\Controllers\caregiverHomeController;
+use App\Http\Controllers\FamilyMemberHomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AppointmentController;
 
 
 
@@ -51,6 +55,26 @@ Route::post('/roster/store', [RosterController::class, 'store'])->name('roster.s
 Route::middleware(['auth', 'doctor'])->group(function () {
     Route::get('/doctor/home', [DoctorHomeController::class, 'index'])->name('doctor.home');
     Route::post('/doctor/submit-till-date', [DoctorHomeController::class, 'submitTillDate'])->name('doctor.submit-till-date');
+});
+
+Route::middleware(['auth', 'caregiver'])->group(function () {
+    Route::get('/caregiver/home', [caregiverHomeController::class, 'index']);
+    Route::post('/caregiver/update-details', [caregiverHomeController::class, 'updateDetails']);
+});
+
+Route::middleware(['auth', 'family_member'])->group(function () {
+    Route::get('/family-member/home', [FamilyMemberHomeController::class, 'index']);
+    Route::post('/family-member/update-details', [FamilyMemberHomeController::class, 'updateDetails']);
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/report', [AdminController::class, 'index']);
+    Route::get('/admin/show-missed-activity', [AdminController::class, 'showMissedActivity']);
+});
+
+Route::middleware(['auth', 'patient'])->group(function () {
+    Route::get('/appointment/create', [AppointmentController::class, 'create']);
+    Route::post('/appointment/store', [AppointmentController::class, 'store']);
 });
 
 // Route::get('/logInRoute', [LoginController::class, 'showLoginForm'])->name('logIn');
